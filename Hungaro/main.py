095fa63cf58a0fd4a8332e504a5f58bf63bbaa28
Juan Genaro García Alanis <88780201+GenaroGA@users.noreply.github.com>
2022-05-20 08:28:02 -0500

    Update main.py

--- Hungaro/main.py
+++ Hungaro/main.py
@@ -2,15 +2,16 @@ import numpy as np
 import hungaro as h
 
 tabla = h.hungaro()
+print(tabla.tabla)
 tabla.minimos_filas()
-tabla.restar_filas
+tabla.restar_filas()
+print(tabla.tabla)
 tabla.minimos_columnas()
 tabla.restar_columnas()
 
 tabla.contar_ceros()
-
 while np.amax(tabla.cerosFilas) > 0 and np.amax(tabla.cerosColumnas) > 0:
     tabla.contar_ceros()
     tabla.lineas()
-print(tabla.tabla_lineas)
+    print(tabla.tabla_lineas)
 print(tabla.tabla)
